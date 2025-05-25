074d0ec0baf8abe391c912d9d230ba243f79478a
Thetzt <122680531+Thetzt@users.noreply.github.com>
2025-05-26 05:07:02 +0630

    Update claim.js

--- pages/api/claim.js
+++ pages/api/claim.js
@@ -3,14 +3,14 @@ export default function handler(req, res) {
   res.status(200).send(`
     <html>
       <head>
-        <meta property="og:title" content="Claim Success!" />
+        <meta property="og:title" content="Claimed!" />
         <meta property="og:image" content="/monad-preview.png" />
         <meta property="fc:frame" content="vNext" />
         <meta property="fc:frame:button:1" content="Back" />
       </head>
       <body>
-        <h1>Claim Successful!</h1>
-        <p>You have received your $MON tokens.</p>
+        <h1>Success</h1>
+        <p>You claimed your tokens.</p>
       </body>
     </html>
   `);
